123628f9a53fccc0f72154a521d0d19880dcbacd
Lorena Silva <162520813+SantosLorenaa@users.noreply.github.com>
2024-12-27 10:19:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+  // Função para abrir o modal
+  function openModal(modalId) {
+    document.getElementById(modalId).classList.remove('hidden');
+  }
+
+  // Função para fechar o modal
+  function closeModal(modalId) {
+    document.getElementById(modalId).classList.add('hidden');
+  }
