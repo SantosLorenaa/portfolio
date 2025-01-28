6f8932e4d38d95e7fbb929858979d0bbe3a4b383
Lorena Silva <162520813+SantosLorenaa@users.noreply.github.com>
2025-01-28 10:10:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
   // Função para abrir o modal
   function openModal(modalId) {
-    document.getElementById(modalId).classList.remove('hidden');
-  }
-
-  // Função para fechar o modal
-  function closeModal(modalId) {
-    document.getElementById(modalId).classList.add('hidden');
-  }
+      document.getElementById(modalId).classList.remove('hidden');
+    }
+    
+    function closeModal(modalId) {
+      document.getElementById(modalId).classList.add('hidden');
+    }
